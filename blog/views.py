ff20e808ede9f176e4cb07df4867c682f7745e72
Raymond Yang <raymondcyang0219@gmail.com>
2021-09-18 12:12:19 +0800

    search-bar: added the search bar

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,13 @@ class ArticleListView(ListView):
     template_name = 'blog/article_list.html'
     ordering = ['-created_date']
 
+    def get_queryset(self):
+        title = self.request.GET.get('title', '')
+        object_list = self.model.objects.all()
+        if title:
+            object_list = object_list.filter(title__icontains=title)
+        return object_list
+
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog/article_detail.html'
