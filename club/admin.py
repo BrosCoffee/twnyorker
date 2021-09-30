978c0733d545c9216f7af3520ba3da4a458545de
Raymond Yang <raymondcyang0219@gmail.com>
2021-09-30 15:27:49 +0800

    english-club: event clicking & hovering

--- club/admin.py
+++ club/admin.py
@@ -3,7 +3,7 @@ from .models import Event
 from pytz import timezone
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('host', 'created_date', 'title', 'get_tags', 'note', 'get_members', 'get_date', 'get_start_time', 'get_end_time', 'signup_deadline', 'max_participants',)
+    list_display = ('host', 'get_date', 'get_start_time', 'get_end_time', 'title', 'get_tags', 'note', 'get_members', 'signup_deadline', 'max_participants',)
     filter_horizontal = ('tags',)
 
     def get_tags(self, obj):
@@ -13,13 +13,13 @@ class EventAdmin(admin.ModelAdmin):
         return ', '.join([member.get_full_name() for member in obj.members.all()])
 
     def get_date(self, obj):
-        return obj.start_time.astimezone(timezone('Asia/Taipei')).strftime('%Y-%m-%d')
+        return obj.start_time.astimezone(timezone('Asia/Taipei')).strftime('%Y/%m/%d')
 
     def get_start_time(self, obj):
-        return obj.start_time.astimezone(timezone('Asia/Taipei')).strftime('%H:%M:%S')
+        return obj.start_time.astimezone(timezone('Asia/Taipei')).strftime('%I:%M %p')
 
     def get_end_time(self, obj):
-        return obj.end_time.astimezone(timezone('Asia/Taipei')).strftime('%H:%M:%S')
+        return obj.end_time.astimezone(timezone('Asia/Taipei')).strftime('%I:%M %p')
 
     get_tags.short_description       = 'Tags'
     get_members.short_description    = 'Members'
--- club/views.py
+++ club/views.py
@@ -1,18 +1,46 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from club.models import Event
+from account.models import SiteUser
 from pytz import timezone
 
 @login_required(login_url='/signin')
 def calendar(request):
+    asia = timezone('Asia/Taipei')
+    if request.is_ajax():
+        event_pk = request.POST.get('eventPk', None)
+        user_pk = request.POST.get('userPk', None)
+        selected_event = Event.objects.filter(pk=event_pk).first() if event_pk else None
+        # TO-DO: User restriction. eq: Youth or Adult
+        user = SiteUser.objects.filter(pk=user_pk).first() if user_pk else None
+        print('user:',user)
+        if selected_event:
+            host = selected_event.host.get_full_name()
+            start_time = selected_event.start_time.astimezone(asia).strftime('%I:%M %p')
+            end_time = selected_event.end_time.astimezone(asia).strftime('%I:%M %p')
+            max_participants = selected_event.max_participants
+            num_participants = len(selected_event.members.all())
+            note = selected_event.note
+            signup_deadline = selected_event.signup_deadline.strftime('%Y/%m/%d') if selected_event.signup_deadline else None
+            return JsonResponse({
+                'host': host,
+                'start_time': start_time,
+                'end_time': end_time,
+                'max_participants': max_participants,
+                'num_participants': num_participants,
+                'note': note,
+                'signup_deadline': signup_deadline,
+            }, safe=False)
     # TO-DO: Not efficient. Revisit it later
     events = Event.objects.all()
     events_list = []
     for event in events:
         events_list.append({
             'title': event.title,
-            'start': event.start_time.astimezone(timezone('Asia/Taipei')).strftime('%Y-%m-%dT%H:%M:%S'),
-            'end': event.end_time.astimezone(timezone('Asia/Taipei')).strftime('%Y-%m-%dT%H:%M:%S'),
+            'eventPk': event.pk,
+            'start': event.start_time.astimezone(asia).strftime('%Y-%m-%dT%H:%M:%S'),
+            'end': event.end_time.astimezone(asia).strftime('%Y-%m-%dT%H:%M:%S'),
         })
     return render(request, 'club/calendar.html', {
         'events_list': events_list,
