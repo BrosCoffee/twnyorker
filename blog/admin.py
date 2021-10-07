671abf21a4da34929294a4a9fcce04128f0a0dc0
Raymond Yang <raymondcyang0219@gmail.com>
2021-10-07 12:39:14 +0800

    ckeditor: added the view button on the article admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Article, Tag
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('author', 'created_date', 'title', 'get_tags', 'introduction', 'content', 'slug',)
+    list_display = ('author', 'view_article', 'created_date', 'title', 'get_tags', 'introduction', 'content', 'slug',)
     filter_horizontal = ('tags',)
     prepopulated_fields = {'slug': ('title',)}
 
     def get_tags(self, obj):
         return ', '.join([tag.title for tag in obj.tags.all()])
 
+    def view_article(self, obj):
+        return mark_safe('<a href="/blog/{}" target="_blank">view</a>'.format(obj.slug))
+
     get_tags.short_description = 'Tags'
+    view_article.short_description = 'Article'
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('title', 'description',)
